05f9bf23cb03f7510d79b04a85b2929a91724872
bibek-thapa <bibek.thaphere1@gmail.com>
2020-02-04 20:34:11 -0600

    Added more comments

--- src/main/java/com/bank/service/BankService.java
+++ src/main/java/com/bank/service/BankService.java
@@ -19,7 +19,7 @@ public class BankService {
 		return accounts;
 	}
 
-	//get account number
+	//get account number of the specific id
 	public Account getAccount(Integer id) {
 		Optional<Account> acct = accounts.stream().filter(acc -> acc.getAccountNumber().equals(id)).findFirst();
 		if (acct.isPresent())
